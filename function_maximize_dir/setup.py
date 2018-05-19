419bc492f75f900624e638d66468f7b54229edd5
Ameya Daigavane <ameya.d.98@gmail.com>
2018-05-20 01:12:40 +0530

    changed URLs

--- function_maximize_dir/setup.py
+++ function_maximize_dir/setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'Genetic algorithms for 2-dimensional function maximization.',
   author = 'Ameya Daigavane',
   author_email = 'ameya.d.98@gmail.com',
-  url = 'https://github.com/ameya98/GeneticAlgorithmsRepo/function_maximize_dir',
-  download_url = 'https://github.com/ameya98/function_maximize_genetic/archive/0.1.tar.gz',
+  url = 'https://github.com/ameya98/GeneticAlgorithmsRepo/tree/master/function_maximize_dir',
+  download_url = 'https://github.com/ameya98/GeneticAlgorithmsRepo/archive/0.1.tar.gz',
   keywords = ['genetic', 'genetic_algorithms'],
   classifiers = [],
 )
