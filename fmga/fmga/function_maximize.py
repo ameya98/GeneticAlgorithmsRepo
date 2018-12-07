20ea2aab896aaf7380d5c35c4d37cad29027db75
Ameya Daigavane <ameya.d.98@gmail.com>
2018-12-07 22:26:37 +0530

    Commenting more clear.

--- fmga/fmga/function_maximize.py
+++ fmga/fmga/function_maximize.py
@@ -5,13 +5,13 @@ Author: Ameya Daigavane
 Date: 15th April, 2018
 '''
 
-# external library dependencies
+# External library dependencies
 from random import randint, uniform
 import numpy as np
 import pathos.multiprocessing as mp
 
 
-# a weighted choice function
+# Weighted choice function - each choice has a corresponding weight
 def weighted_choice(choices, weights):
     normalized_weights = np.array([weight for weight in weights]) / np.sum(weights)
     threshold = uniform(0, 1)
@@ -26,7 +26,7 @@ def weighted_choice(choices, weights):
 # Point class and method definitions
 class Point:
 
-    # create random n-dimensional point within boundaries
+    # Create random n-dimensional point within boundaries
     def __init__(self, associated_population=None, dimensions=2):
 
         if associated_population is None:
@@ -46,7 +46,7 @@ class Point:
         self.fitness_rank = -1
         self.diversity_rank = -1
 
-    # fitness score - objective function evaluated at the point
+    # Fitness score - objective function evaluated at the point
     def evaluate_fitness(self, eval_function=None):
         try:
             self.fitness = eval_function(*self.coordinates)
@@ -55,7 +55,7 @@ class Point:
             print("function passed is invalid.")
             raise
 
-    # mutation
+    # Mutation operator
     def mutate(self):
         # choose the index at random
         index = randint(0, np.size(self.coordinates) - 1)
@@ -75,6 +75,7 @@ class Population:
                  elite_fraction=0.1, mutation_probability=0.05, mutation_range=5, verbose=0,
                  multiprocessing=False, processes=8):
 
+        # Data-validation for parameters
         if elite_fraction > 1.0 or elite_fraction < 0.0:
             raise ValueError("Parameter 'elite_fraction' must be in range [0,1].")
 
@@ -130,14 +131,14 @@ class Population:
         self.num_iterations = 1
         self.multiprocessing = multiprocessing
 
-        # create points
+        # Create points
         for pointnumber in range(self.size):
             point = Point(associated_population=self, dimensions=self.num_dimensions)
             self.points.append(point)
             self.points[pointnumber].index = pointnumber
 
+        # If multiprocessing is enabled, create pool of processes.
         if self.multiprocessing:
-            # create pool of processes
             if processes is None:
                 self.pool = mp.ProcessingPool()
             else:
@@ -145,18 +146,18 @@ class Population:
 
             fitnesses = self.pool.map(lambda coordinates, func: func(*coordinates), [point.coordinates for point in self.points], [self.objective_function] * self.size)
 
-            # assign fitnesses to each point
+            # Assign fitnesses to each point
             for index, point in enumerate(self.points):
                 point.fitness = fitnesses[index]
         else:
             for point in self.points:
                 point.evaluate_fitness(self.objective_function)
 
-        # evaluate the ranks
+        # Evaluate fitness and diversity ranks
         self.__evaluate_fitness_ranks()
         self.__evaluate_diversity_ranks()
 
-    # evaluate fitness rank of each point in population
+    # Evaluate the fitness rank of each point in the population
     def __evaluate_fitness_ranks(self):
         if not self.evaluated_fitness_ranks:
             self.mean_fitness = np.sum(point.fitness for point in self.points) / self.size
@@ -168,10 +169,10 @@ class Population:
 
             self.evaluated_fitness_ranks = True
 
-    # evaluate diversity rank of each point in population
+    # Evaluate the diversity rank of each point in the population
     def __evaluate_diversity_ranks(self):
         if not self.evaluated_diversity_ranks:
-            # find mean coordinates
+            # Find mean coordinates
             self.mean_coordinates = np.sum(point.coordinates for point in self.points) / self.size
 
             for point in self.points:
@@ -185,46 +186,46 @@ class Population:
 
             self.evaluated_diversity_ranks = True
 
-    # generate the new population by breeding points
+    # Generate the new population by breeding points
     def __breed(self):
-        # sort according to fitness rank
+        # Sort according to fitness rank
         self.points.sort(key=lambda point: point.fitness_rank)
 
-        # push all the really good points first
+        # Push all the really good points first (according to fitness)
         newpopulation = []
         for pointnumber in range(self.elite_population_size):
             newpopulation.append(self.points[pointnumber])
 
-        # assign weights to being selected for breeding
+        # Assign weights to being selected for breeding
         weights = [1 / (1 + point.fitness_rank + point.diversity_rank) for point in self.points]
 
-        # randomly select for the rest and breed
+        # Randomly select for the rest and breed
         while len(newpopulation) < self.size:
             parent1 = weighted_choice(list(range(self.size)), weights)
             parent2 = weighted_choice(list(range(self.size)), weights)
 
-            # don't breed with yourself, dude!
+            # Don't breed with yourself, dude!
             while parent1 == parent2:
                 parent1 = weighted_choice(list(range(self.size)), weights)
                 parent2 = weighted_choice(list(range(self.size)), weights)
 
-            # breed now
+            # Breed now
             child1, child2 = crossover(self.points[parent1], self.points[parent2])
 
-            # add the children
+            # Add the children
             newpopulation.append(child1)
             if len(newpopulation) < self.size:
                 newpopulation.append(child2)
 
-        # assign the new population
+        # Re-assign to the new population
         self.points = newpopulation
 
-        # evaluate fitnesses of new population points
+        # Evaluate fitnesses of new population points
         if self.multiprocessing:
-            # reuse process pool
+            # Reuse pool of processes
             fitnesses = self.pool.map(lambda coordinates, func: func(*coordinates), [point.coordinates for point in self.points], [self.objective_function] * self.size)
 
-            # assign fitnesses to each point
+            # Assign fitnesses to each point
             for index, point in enumerate(self.points):
                 point.fitness = fitnesses[index]
         else:
@@ -237,25 +238,27 @@ class Population:
     # mutate population randomly
     def __mutate(self):
         for point in self.points:
-            test_probability = uniform(0, 1)
-            if test_probability < self.mutation_probability:
+            mutate_probability = uniform(0, 1)
+            if mutate_probability < self.mutation_probability:
                 point.mutate()
                 point.evaluate_fitness(self.objective_function)
 
                 self.evaluated_fitness_ranks = False
                 self.evaluated_diversity_ranks = False
 
-    # perform one iteration
+    # Perform one iteration of breeding and mutation
     def iterate(self):
-        # breed and mutate
+        # Breed
         self.__breed()
+
+        # Mutate
         self.__mutate()
 
-        # find the new ranks
+        # Find the new population's fitness and diversity ranks
         self.__evaluate_fitness_ranks()
         self.__evaluate_diversity_ranks()
 
-        # print the stats
+        # Print the population stats, if enabled
         if self.verbose == 1:
             print("Iteration", self.num_iterations, "complete.")
         elif self.verbose == 2:
@@ -266,12 +269,12 @@ class Population:
 
         self.num_iterations += 1
 
-    # perform the iterations sequentially
+    # Perform iterations sequentially
     def converge(self, iterations=15):
         for iteration in range(1, iterations + 1):
             self.iterate()
 
-    # point with best fitness is the estimate of point of maxima
+    # The point with best fitness is the estimate of point of maxima
     def best_estimate(self):
         best_point_fitness = float("-inf")
         best_point = None
@@ -283,7 +286,7 @@ class Population:
         return best_point
 
 
-# crossover (breed) 2 points by swapping x-coordinates
+# Crossover (breed) 2 points by swapping coordinates
 def crossover(point1, point2):
     if point1.associated_population != point2.associated_population:
         raise ValueError("Points are from different populations.")
@@ -298,17 +301,20 @@ def crossover(point1, point2):
 
     return child1, child2
 
-# wrapper for the building of the population and converging
+
+# Wrapper to build a population and converge to function maxima.
 def maximize(objective_function=None, dimensions=None, population_size=60, boundaries=None, elite_fraction=0.1,
              mutation_probability=0.05, mutation_range=5, verbose=0, multiprocessing=False, processes=None, iterations=15):
 
-    population = Population(objective_function, dimensions, population_size, boundaries, elite_fraction,
-                            mutation_probability, mutation_range, verbose, multiprocessing, processes)
+    population = Population(objective_function=objective_function, dimensions=dimensions, population_size=population_size,
+                            boundaries=boundaries, elite_fraction=elite_fraction, mutation_probability=mutation_probability,
+                            mutation_range=mutation_range, verbose=verbose, multiprocessing=multiprocessing, processes=processes)
+
     population.converge(iterations)
     return population.best_estimate()
 
 
-# helper to unpack arguments with shapes as given
+# Helper to unpack arguments with shapes as given
 def unpack(args, shapes):
     try:
         np_args = np.array(args)
